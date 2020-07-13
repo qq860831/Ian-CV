3a3107215c1a9b1d3187c067be9e7281d4c25bdd
qq860831 <qq860831@gmail.com>
2020-07-13 17:37:57 +0800

    ok

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,11 @@
+// jQuery的初始化
 $(document).ready(function(){
 
 	// header color change
-	$(window).on("scroll",function(){
+	$(window).on("scroll",function(){  // 頁面在滾動時
 		var scrollDistance = $(window).scrollTop();
 		var $header = $(".js-header");
-		if(scrollDistance > 80){
+		if(scrollDistance > 80){  //80剛好就是離開上面有人物的地方
 			$header.addClass("header--colored");
 		}
 		else{
